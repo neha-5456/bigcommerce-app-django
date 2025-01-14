0860e124426218e2cd98e2e42fedd9f9bf3f7e81
officialtpss <officialtpss@gmail.com>
2025-01-14 12:21:35 +0530

    url

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ from app.views import index, test
 urlpatterns = [
     path('', index),
     path('/test/', test),
-    path('/auth/', views.bigcommerce_auth, name='bigcommerce_auth'),
-    path('/auth/callback/', views.auth_callback, name='auth_callback'),
-    path('/products/', views.products, name='products'),
+    path('auth/', views.bigcommerce_auth, name='bigcommerce_auth'),
+    path('auth/callback/', views.auth_callback, name='auth_callback'),
+    path('products/', views.products, name='products'),
 ]
