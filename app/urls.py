c359377ca5a641790fd1af7bb3c2dde7d64f0207
officialtpss <officialtpss@gmail.com>
2025-01-14 12:25:12 +0530

    urbl

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-from app.views import index, test
+# from app.views import index, test
 
 
 urlpatterns = [
-    path('', index),
-    path('/test/', test),
+    path('', views.index),
+    path('/test/', views.test),
     # path('auth/', views.bigcommerce_auth, name='bigcommerce_auth'),
     # path('auth/callback/', views.auth_callback, name='auth_callback'),
     # path('products/', views.products, name='products'),
