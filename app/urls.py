e64fc1c89fd59e65dd24c09473863ab5b6b97d97
officialtpss <officialtpss@gmail.com>
2025-01-16 07:09:20 +0530

    check auth uri

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from app.views import index, test , install , auth_callback , custom_tab
+from app.views import  test , install , auth_callback 
 
 
 urlpatterns = [
@@ -8,6 +8,6 @@ urlpatterns = [
     path('test/', test),
     # path('install/', install, name='install'),
     path('auth/callback/', auth_callback, name='auth_callback'),
-    path('custom-tab/', custom_tab, name='custom_tab'),
+   
     
 ]
