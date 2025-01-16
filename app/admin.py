ce384df4ee6cdf08b61932df5974f502475317b2
officialtpss <officialtpss@gmail.com>
2025-01-16 09:50:36 +0530

    display tabel on admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import StoreData
 
-# Register your models here.
+class StoreDataAdmin(admin.ModelAdmin):
+    list_display = ('store_hash', 'access_token', 'user_email','created_at','updated_at')  # Fields to display in the list view
+    search_fields = ('user_email',)  # Add search functionality for the 'name' field
+
+admin.site.register(StoreData, StoreDataAdmin)
