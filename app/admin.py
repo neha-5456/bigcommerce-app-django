709239ccad2b031c25989c8c5e36b86925da1486
officialtpss <officialtpss@gmail.com>
2025-01-17 07:41:11 +0530

    tabels

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-# from .models import StoreData
+from .models import Store , StoreUser ,User
 
-# class StoreDataAdmin(admin.ModelAdmin):
-#     list_display = ('store_hash', 'access_token', 'user_email','created_at','updated_at')  # Fields to display in the list view
-#     search_fields = ('user_email',)  # Add search functionality for the 'name' field
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('store_hash', 'access_token', 'scope')  
+    search_fields = ('store_hash') 
 
-# admin.site.register(StoreData, StoreDataAdmin)
+admin.site.register(Store, StoreAdmin)
+class StoreUserAdmin(admin.ModelAdmin):
+    list_display = ('store', 'user', 'admin')  
+    search_fields = ('user') 
+    
+    
+admin.site.register(StoreUser, StoreUserAdmin)
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('bc_id', 'email')  
+    search_fields = ('email') 
+    
+    
+admin.site.register(User, UserAdmin)
