cf4952a4fe809359f47f0b752b2627d333dce10a
officialtpss <officialtpss@gmail.com>
2025-01-29 13:51:30 +0530

    db

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,7 @@ from app.views import  index , install , load
 urlpatterns = [
     # path('', install),
     path('', index),
+    
     path('auth/callback/', install, name='auth_callback'),
     path('load/', load, name='load'),
    
